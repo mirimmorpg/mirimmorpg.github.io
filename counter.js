fc7a24e2a2164b1af822ab947ee22bfdb11ecf07
Miriam <miriamtanski@googlemail.com>
2021-06-07 22:14:49 +0200

    set right time

--- counter.js
+++ counter.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Jun 09, 2021 00:00:00",
+  let birthday = "Jun 09, 2021 05:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {
 
